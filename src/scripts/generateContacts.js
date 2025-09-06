a994f1c47e1a7c8a1ae0d12a93b43c7d45c54a9a
Alof Bronco <130389810+AlofBronco@users.noreply.github.com>
2025-09-06 13:39:35 +0300

    done

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,14 @@
-const generateContacts = async (number) => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+const generateContacts = async (number) => {
+  const data = await readContacts();
+  for (let i = 0; i < number; i++) {
+    const fakeContact = createFakeContact();
+    data.push(fakeContact);
+  }
+  await writeContacts(data);
+};
 
 generateContacts(5);
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,7 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (updatedContacts) => {
+  const data = await fs.writeFile(PATH_DB, JSON.stringify(updatedContacts));
+  return data;
+};

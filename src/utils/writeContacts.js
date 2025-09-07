7f0d7ebecaafee2bd29ffffb13117e9889ac7828
Alof Bronco <130389810+AlofBronco@users.noreply.github.com>
2025-09-07 12:05:28 +0300

    fix

--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,7 +1,10 @@
 import { PATH_DB } from '../constants/contacts.js';
 import fs from 'node:fs/promises';
+import path from 'node:path';
+
+const PATH = path.resolve(PATH_DB);
 
 export const writeContacts = async (updatedContacts) => {
-  const data = await fs.writeFile(PATH_DB, JSON.stringify(updatedContacts));
+  const data = await fs.writeFile(PATH, JSON.stringify(updatedContacts));
   return data;
 };

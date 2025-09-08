f1edc53481d7fbd1172066008a35398cfd8e39bb
Alof Bronco <130389810+AlofBronco@users.noreply.github.com>
2025-09-08 16:46:28 +0300

    fix

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -2,7 +2,7 @@ import { PATH_DB } from '../constants/contacts.js';
 import fs from 'node:fs/promises';
 import path from 'node:path';
 
-const PATH = path.resolve(PATH_DB);
+const PATH = path.resolve(process.cwd(), PATH_DB);
 
 export const readContacts = async () => {
   const data = await fs.readFile(PATH, 'utf-8');

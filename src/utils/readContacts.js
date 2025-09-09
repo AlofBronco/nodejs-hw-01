eb790b9591db3bf808b29f51edea5d5ee6120647
Alof Bronco <130389810+AlofBronco@users.noreply.github.com>
2025-09-09 16:42:04 +0300

    fix

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,10 +1,7 @@
 import { PATH_DB } from '../constants/contacts.js';
 import fs from 'node:fs/promises';
-import path from 'node:path';
-
-const PATH = path.resolve(process.cwd(), PATH_DB);
 
 export const readContacts = async () => {
-  const data = await fs.readFile(PATH, 'utf-8');
+  const data = await fs.readFile(PATH_DB, 'utf-8');
   return JSON.parse(data);
 };
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,10 +1,7 @@
 import { PATH_DB } from '../constants/contacts.js';
 import fs from 'node:fs/promises';
-import path from 'node:path';
-
-const PATH = path.resolve(process.cwd(), PATH_DB);
 
 export const writeContacts = async (updatedContacts) => {
-  const data = await fs.writeFile(PATH, JSON.stringify(updatedContacts));
+  const data = await fs.writeFile(PATH_DB, JSON.stringify(updatedContacts));
   return data;
 };
